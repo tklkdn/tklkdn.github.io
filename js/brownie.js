f008b70ccbfc6bf33d34e2c30db01596f65fab7e
Taka Kodani <tklkdn@gmail.com>
2018-08-12 00:05:06 -0500

    Removed extraneous code

--- js/brownie.js
+++ js/brownie.js
@@ -27,13 +27,3 @@ $(document).ready(function(){
 		.setTween(wipeAnimation)
 		.addTo(controller);
 });
-
-var $root = $('html, body');
-
-$('a[href^="#"]').click(function () {
-    $root.animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-
-    return false;
-});
