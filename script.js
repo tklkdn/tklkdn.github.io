bad58b61f43c353288658c4f7e69325c6d1c59c5
Taka Kodani <tklkdn@gmail.com>
2021-12-23 08:57:53 -0800

    update

--- script.js
+++ script.js
@@ -3,17 +3,6 @@ $(document).ready(function(){
 	  // Your custom settings go here
 	});
 	
-	$(".bg-1").hide();
-	$(".bg-2").hide();
-	$(".bg-3").hide();
-	$(".bg-4").hide();
-	$(".bg-5").hide();
-	$(".bg-6").hide();
-	$(".bg-7").hide();
-	$(".container").hide();
-	
-	let bgArr = [".main", ".bg-1", ".bg-2", ".bg-3", ".bg-4", ".bg-5", ".bg-6", ".bg-7"];
-	
 	function hideAllExcept(bg) {
 		$(bg).fadeIn(500);
 		Array.prototype.forEach.call(bgArr, function(i) {
@@ -28,97 +17,19 @@ $(document).ready(function(){
 		}
 	}
 	
-	$("#link-0").click(function() {
-		hideAllExcept(".main");
-	});
+	let bgArr = [".main", ".bg-1", ".bg-2", ".bg-3", ".bg-4", ".bg-5", ".bg-6", ".bg-7"];
 	
-	$("#link-1").click(function() {
-		hideAllExcept(".bg-1");
-	});
+	let links = [];
+	for (i = 0; i < 8; i++) {
+		links.push(`#link-${i}`);
+	}
 	
-	$("#link-2").click(function() {
-		hideAllExcept(".bg-2");
-	});
-	
-	$("#link-3").click(function() {
-		hideAllExcept(".bg-3");
-	});
-	
-	$("#link-4").click(function() {
-		hideAllExcept(".bg-4");
-	});
-	
-	$("#link-5").click(function() {
-		hideAllExcept(".bg-5");
-	});
-	
-	$("#link-6").click(function() {
-		hideAllExcept(".bg-6");
-	});
-	
-	$("#link-7").click(function() {
-		hideAllExcept(".bg-7");
-	});
-	
-	/*
-	$(".sidenav").mouseenter(function() {
-		$(".main").fadeOut(500);
-		$("#home a").css("color", "white");
-	});
-	$(".sidenav").mouseleave(function() {
-		$(".main").fadeIn(500);
-		$("#home a").css("color", "black");
-	});
+	for (i = 0; i < 8; i++) {
+		$(`${links[i]}`).on("click", {"idx": i}, function(e) {
+			hideAllExcept(bgArr[e.data.idx]);
+		});
+	}
 
-	$("#link-1").mouseenter(function() {
-		$(".bg-1").fadeIn(500);
-	});
-	$("#link-1").mouseleave(function() {
-		$(".bg-1").fadeOut(500);
-	});
-
-	$("#link-2").mouseenter(function() {
-		$(".bg-2").fadeIn(500);
-	});
-	$("#link-2").mouseleave(function() {
-		$(".bg-2").fadeOut(500);
-	});
-
-	$("#link-3").mouseenter(function() {
-		$(".bg-3").fadeIn(500);
-	});
-	$("#link-3").mouseleave(function() {
-		$(".bg-3").fadeOut(500);
-	});
-
-	$("#link-4").mouseenter(function() {
-		$(".bg-4").fadeIn(500);
-	});
-	$("#link-4").mouseleave(function() {
-		$(".bg-4").fadeOut(500);
-	});
-
-	$("#link-5").mouseenter(function() {
-		$(".bg-5").fadeIn(500);
-	});
-	$("#link-5").mouseleave(function() {
-		$(".bg-5").fadeOut(500);
-	});
-
-	$("#link-6").mouseenter(function() {
-		$(".bg-6").fadeIn(500);
-	});
-	$("#link-6").mouseleave(function() {
-		$(".bg-6").fadeOut(500);
-	});
-
-	$("#link-7").mouseenter(function() {
-		$(".bg-7").fadeIn(500);
-	});
-	$("#link-7").mouseleave(function() {
-		$(".bg-7").fadeOut(500);
-	});
-	*/
 	refreshData();
 });
 
